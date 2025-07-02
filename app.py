cfc162fbef9bc6547e4888c505f4e7a17161ec00
Denis <103510644+YDJUDGE@users.noreply.github.com>
2025-07-02 17:40:59 +0300

    Add custom_pages

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-from heroes.main_functions import is_win, format_duration, calculate_kda
-from heroes.special_functions import get_contribution_label
-from heroes.chart import plot_kda_stats
-from datetime import datetime
-from heroes.main_function import fetch_matches
-from pages.hero_analysis import hero_analysis_page
-from pages.match_details import match_details_page
+from pages_custom.hero_analysis import hero_analysis_page
+from pages_custom.match_details import match_details_page
 
 def app():
     st.set_page_config(page_title="Dota 2 Анализ", layout="wide")
